8d69c1c5a4e1d248f08a68a1019293ee3167807f
MityaEgorov <d.egorov@propellerads.net>
2023-07-24 19:52:10 +0200

    Задача 2

--- src/main/java/ru/auto/test/Qwerty.java
+++ src/main/java/ru/auto/test/Qwerty.java
@@ -4,9 +4,9 @@ package ru.auto.test;
 public class Qwerty {
 	public static void main(String[] args) {
 
-		Point r = new Point (6,12);
+		Point r = new Point (12,6);
 
-		System.out.println(" Площадь " + r.p1 + " И " + r.p2 + " что-то " + r.sqrt() );
+		System.out.println(" Расстояние между точками " + r.p1 + " И " + r.p2 + " = " + r.sqrt() );
 
 	}
 
