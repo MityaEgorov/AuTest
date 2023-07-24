d478e2bf7931629a5810c16895f88cc2b3c4eea2
MityaEgorov <d.egorov@propellerads.net>
2023-07-24 18:23:56 +0200

    Новая тема

--- src/main/java/ru/auto/test/Qwerty.java
+++ src/main/java/ru/auto/test/Qwerty.java
@@ -2,6 +2,30 @@ package ru.auto.test;
 
 public class Qwerty {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("Name");
+		hello("User");
+
+		double len = 5;
+		System.out.println("Площадь" + len + "что-то" + area(len) );
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь" + a + "И" + b + "что-то" + area(a, b) );
+
 	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello," + somebody + "!");
+	}
+
+
+	public static double area ( double l) {
+		return 1 * 1 ;
+
+	}
+	public static double area (double a , double b) {
+		return a * b;
+	}
+
 }
