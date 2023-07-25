55bb443bee871ab089e38702bf2bfac7ae2a3308
MityaEgorov <d.egorov@propellerads.net>
2023-07-25 14:35:37 +0200

    FirstTest

---
+++ src/test/java/ru/auto/test/PointTests.java
@@ -0,0 +1,13 @@
+package ru.auto.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testArea(){
+    Point r = new Point (12,6);
+    Assert.assertEquals(r.sqrt(),6.0);
+  }
+}
