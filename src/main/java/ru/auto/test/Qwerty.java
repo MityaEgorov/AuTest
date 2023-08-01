ef03c4e0225b5e2df0ac320c4fe70adf1932a309
MityaEgorov <d.egorov@propellerads.net>
2023-08-01 16:46:49 +0200

    Fix

--- src/main/java/ru/auto/test/Qwerty.java
+++ src/main/java/ru/auto/test/Qwerty.java
@@ -8,18 +8,15 @@ public class Qwerty {
 
 		Point p1 = new Point(12, 6);
 		Point p2 = new Point(35, 45);
-		double result = distance(p1, p2);
+	  double result = distance(p1, p2);
 
 
-		System.out.println(" Расстояние между точками " + p1.x + " И " + p2.y + " = " + result);
+		System.out.println(" Расстояние между точками = " + result);
 
 	}
 
 	public static double distance(Point p1, Point p2) {
-
-		double diffX = (p1.x - p2.x) * (p1.x - p2.x);
-		double diffY = (p1.y - p2.y) * (p1.y - p2.y);
-		return Math.sqrt(diffX + diffY);
+		return (Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y)));
 	}
 }
 
